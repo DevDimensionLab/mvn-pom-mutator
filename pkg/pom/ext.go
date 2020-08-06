fbfeb6e964edecc2614d54374f63a3aed8039634
Alexander Skjolden <alex@plastboks.net>
2020-08-06 09:51:35 +0200

    adds extention functions for write model, and set key in "any" list

--- pkg/pom/ext.go
+++ pkg/pom/ext.go
@@ -2,6 +2,8 @@ package pom
 
 import (
 	"errors"
+	"github.com/perottobc/mvn-pom-mutator/pkg/mvn_crud"
+	"io/ioutil"
 )
 
 func (deps Dependencies) FindArtifact(artifactId string) (Dependency, error) {
@@ -23,4 +25,29 @@ func (any Any) FindKey(key string) (string, error) {
 	}
 
 	return "", errors.New("could not find key " + key + " in any structure")
+}
+
+func (any *Any) SetKey(key string, value string) error {
+	for _, a := range any.AnyElements {
+		if a.XMLName.Local == key {
+			a.Value = value
+			return nil
+		}
+	}
+
+	return errors.New("could not find key " + key + " in any structure")
+}
+
+func (model *Model) WriteToFile(outputFile string) error {
+	bytes, err := mvn_crud.Marshall(model)
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(outputFile, bytes, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
